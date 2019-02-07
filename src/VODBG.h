e087a528e72f13c870b2b7c2866cfff6ed440282
Pierre-Morisse <pierre.morisse2@univ-rouen.fr>
2019-02-07 14:05:03 +0100

    Remove lots of useless files

--- src/VODBG.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <unordered_map>
-#include <vector>
-#include <set>
-#include <algorithm>
-#include <iostream>
-
-int link(std::unordered_map<int, std::unordered_map<std::string, unsigned>> unordered_mapMerCounts, std::string srcSeed, std::string tgtSeed, unsigned curK, std::set<std::string> &visited, unsigned* curBranches, unsigned dist, std::string curExt, std::string &missingPart, unsigned merSize, unsigned LRLen, unsigned maxBranches, unsigned solidThresh, unsigned minOrder);
--- src/localAlignment.h
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-#include<string>
-#include <fstream>
-
-std::pair<int**, std::pair<int, int>> NeedlemanWunschLocalMatrix(std::string s1, std::string s2);
-
-std::pair<std::pair<int, int>, std::pair<int, int>> NeedlemanWunschLocalAlignments(std::string s1, std::string s2);
