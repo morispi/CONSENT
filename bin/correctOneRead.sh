479cb2a4df15b8932d303d5960045626372a10f5
Pierre-Morisse <pierre.morisse2@univ-rouen.fr>
2019-02-07 13:40:37 +0100

    Removed useless bin files

--- bin/correctOneRead.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#echo $(wc -l $8/ExplodedAlignments/$9)
-
-./bin/LRSelfCorrection -a "$8/ExplodedAlignments/$9" -d "$8"/RawLongReads/ -s "$1" -l "$2" -k "$3" -c "$4" -f "$5" -m "$6" -j "$7"
--- bin/parallelCorrection.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-parallel -j "$8" ./bin/correctOneRead.sh "$2" "$3" "$4" "$5" "$6" "$7" 1 "$1" :::: "$1"/ListAlignments >> "$9"
