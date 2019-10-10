78e1363aa1cf96d582d821ebbc669f11b6a22526
Pierre-Morisse <pierre.morisse2@univ-rouen.fr>
2019-10-10 22:34:50 +0200

    Update minScore default value

--- src/CONSENT.cpp
+++ src/CONSENT.cpp
@@ -618,10 +618,10 @@ std::pair<std::string, std::string> processRead(int id, std::vector<Alignment>& 
 	if (doTrimRead) {
 		// std::cerr << "before trim : " << correctedRead.length() << std::endl;
 		correctedRead = trimRead(correctedRead, 1);
-		// std::vector<std::string> splits = splitRead(readId, correctedRead, pilesPos, windowSize, windowOverlap);
-		// for (std::string s : splits) {
-		// 	std::cerr << ">id" << std::endl << s << std::endl;
-		// }
+		std::vector<std::string> splits = splitRead(readId, correctedRead, pilesPos, windowSize, windowOverlap);
+		for (std::string s : splits) {
+			std::cerr << ">id" << std::endl << s << std::endl;
+		}
 		// if (!dropRead(correctedRead)) {
 			// std::cerr << "kept read : " << readId << std::endl << "support was : " << (float) nbCorBases(correctedRead) / correctedRead.length() << std::endl;
 			// std::cerr << "length was : " << correctedRead.length() << std::endl;
@@ -665,7 +665,7 @@ std::vector<Alignment> getReadPile(std::ifstream& alignments, std::string curTpl
 	unsigned i;
 	Alignment curAl;
 	unsigned posMin = 0;
-	unsigned minScore = 0;
+	unsigned minScore = 1000000;
 
 
 	for (std::string o : offsets) {
@@ -788,8 +788,8 @@ void runCorrection(std::string PAFIndex, std::string alignmentFile, unsigned min
 
 	int poolSize = 1000;
 	ctpl::thread_pool myPool(nbThreads);
-	int jobsToProcess = 1000000000;
-	// int jobsToProcess = 500;
+	// int jobsToProcess = 1000000000;
+	int jobsToProcess = 500;
 	int jobsLoaded = 0;
 	int jobsCompleted = 0;
 
