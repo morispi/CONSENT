296e0f47380aec2c325d94e4aec8d4fd3aad2d27
Pierre-Morisse <pierre.morisse2@univ-rouen.fr>
2020-04-24 10:14:16 +0200

    Code cleanup for assembly polishing

--- src/CONSENT-polishing.h
+++ src/CONSENT-polishing.h
@@ -14,10 +14,9 @@
 #include <iostream>
 #include <unistd.h>
 #include <map>
+#include "utils.h"
+#include "Overlap.h"
 
-// Polishing
+std::pair<std::string, std::string> processContig(std::vector<Overlap>& alignments, unsigned minSupport, unsigned maxSupport, unsigned windowSize, unsigned merSize, unsigned commonKMers, unsigned minAnchors, unsigned solidThresh, unsigned windowOverlap, unsigned maxMSA, std::string path, unsigned nbThreads);
 
-std::pair<std::string, std::unordered_map<kmer, unsigned>> computeConsensuses(int id, std::string& readId, std::vector<std::string>& piles, std::pair<unsigned, unsigned>& pilesPos, unsigned& minSupport, unsigned& merSize, unsigned& commonKMers, unsigned& minAnchors, unsigned& solidThresh, unsigned& windowSize, unsigned maxMSA, std::string path, unsigned nbThreads);
-
-std::pair<std::string, std::string> processRead(std::vector<Alignment>& alignments, unsigned minSupport, unsigned maxSupport, unsigned windowSize, unsigned merSize, unsigned commonKMers, unsigned minAnchors, unsigned solidThresh, unsigned windowOverlap, unsigned maxMSA, std::string path, unsigned nbThreads);
-
+void runCorrection(std::string PAFIndex, std::string alignmentFile, unsigned minSupport, unsigned maxSupport, unsigned windowSize, unsigned merSize, unsigned commonKMers, unsigned minAnchors, unsigned solidThresh, unsigned windowOverlap, unsigned nbThreads, std::string readsFile, std::string proofFile, unsigned maxMSA, std::string path);
