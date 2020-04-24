9bbb27a8d913cf21531b026831216070c81febd3
Pierre-Morisse <pierre.morisse2@univ-rouen.fr>
2020-04-24 10:13:57 +0200

    Code cleanup for long read correction

--- src/CONSENT-correction.h
+++ src/CONSENT-correction.h
@@ -4,8 +4,6 @@
 #include <sstream>
 #include <utility>
 #include <vector>
-#include <set>
-#include <algorithm> 
 #include <string>
 #include <utility>
 #include <vector>
@@ -14,9 +12,9 @@
 #include <iostream>
 #include <unistd.h>
 #include <map>
+#include "utils.h"
+#include "Overlap.h"
 
-// Correction
+std::pair<std::string, std::string> processRead(int id, std::vector<Overlap>& alignments, unsigned minSupport, unsigned maxSupport, unsigned windowSize, unsigned merSize, unsigned commonKMers, unsigned minAnchors, unsigned solidThresh, unsigned windowOverlap, unsigned maxMSA, std::string path);
 
-std::pair<std::string, std::unordered_map<kmer, unsigned>> computeConsensuses(std::string& readId, std::vector<std::string>& piles, std::pair<unsigned, unsigned>& pilesPos, unsigned& minSupport, unsigned& merSize, unsigned& commonKMers, unsigned& minAnchors, unsigned& solidThresh, unsigned& windowSize, unsigned maxMSA, std::string path);
-
-std::pair<std::string, std::string> processRead(int id, std::vector<Alignment>& alignments, unsigned minSupport, unsigned maxSupport, unsigned windowSize, unsigned merSize, unsigned commonKMers, unsigned minAnchors, unsigned solidThresh, unsigned windowOverlap, unsigned maxMSA, std::string path);
+void runCorrection(std::string PAFIndex, std::string alignmentFile, unsigned minSupport, unsigned maxSupport, unsigned windowSize, unsigned merSize, unsigned commonKMers, unsigned minAnchors, unsigned solidThresh, unsigned windowOverlap, unsigned nbThreads, std::string readsFile, std::string proofFile, unsigned maxMSA, std::string path);
